d9974db393c30b6a6d0d5a90696b944c31345e46
rachmadandy <rachmadandyms@gmail.com>
2024-04-15 09:07:07 +0700

    Add dropdown menu for `navbarhome.blade.php`

--- resources/views/layouts/navbarhome.blade.php
+++ resources/views/layouts/navbarhome.blade.php
@@ -15,12 +15,34 @@
                 <img src="{{ asset('images/logos/logo2.png') }}" alt="logo2">
             </div>
         </div>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav mx-auto">
-                <a class="nav-link text-dark" href="#">Beranda</a>
-                <a class="nav-link text-dark" href="#">Tentang</a>
-                <a class="nav-link text-dark" href="/event">Event</a>
-            </div>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Beranda</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Destinasi Wisata
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#" class="dropdown-item">Wisata Alam</a></li>
+                        <li><a href="#" class="dropdown-item">Wisata Petualangan</a></li>
+                        <li><a href="#" class="dropdown-item">Wisata Perbelanjaan</a></li>
+                        <li><a href="#" class="dropdown-item">Wisata Kuliner</a></li>
+                        <li><a href="#" class="dropdown-item">Wisata Industri</a></li>
+                        <li><a href="#" class="dropdown-item">Wisata Budaya</a></li>
+                        <li><a href="#" class="dropdown-item">Wisata Pertanian</a></li>
+                        <li><a href="#" class="dropdown-item">Wisata Perdesaan</a></li>
+                        <li><a href="#" class="dropdown-item">Wisata Religi</a></li>
+                        <li><a href="#" class="dropdown-item">Wisata Industri Kecil</a></li>
+                        <li><a href="#" class="dropdown-item">Wisata Edukasi</a></li>
+                        <li><a href="#" class="dropdown-item">Wisata Buatan</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/event">Event</a>
+                </li>
+            </ul>
         </div>
         <div class=" gap-3 d-none d-lg-flex">
             <div class="logo-right">
