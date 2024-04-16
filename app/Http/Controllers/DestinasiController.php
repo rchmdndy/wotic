3c78f27543ab455a074a1277b6bcc370012896e1
rachmadandy <rachmadandyms@gmail.com>
2024-04-16 17:44:39 +0700

    feat: Create  API Controller for Destination, update destination list view, and add api route
    - Create "DestinationApiController.php" for Destination API Controller
    - Update "list.blade.php" to use AJAX to process data from API
    - Implement destination API route in "api.php"
    
    These changes enhance user experience by providing authentication functionality, improving profile page aesthetics, and adding a convenient password reset feature.

--- app/Http/Controllers/DestinasiController.php
+++ app/Http/Controllers/DestinasiController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Destinasi;
 
-class DestinasiController extends Controller
+class   DestinasiController extends Controller
 {
     public function index(){
         return view('destination.index');
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('/v1')->group(function (){
+    Route::prefix('destination')->group(function(){
+         Route::get('/getAll', [\App\Http\Controllers\Api\DestinationApiController::class, 'getAllDestination'])->name('api.destination.all') ;
+    });
+//   Route::get();
+});
