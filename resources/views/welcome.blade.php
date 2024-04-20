f0c77a439d085de97312ee95866e7c6d3e8a8760
imarif69 <osis.ilham99@gmail.com>
2024-04-21 05:42:55 +0700

    [PUSH] replace home page with new template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,17 +3,38 @@
 @section('content')
     {{-- Hero Section --}}
     <section class="hero">
-        <video autoplay muted loop class="video-hero">
-            <source src="{{ asset('videos/video.mp4') }}" type="video/mp4" />
-            Your browser does not support the video tag.
-        </video>
-        <div class="d-flex flex-column align-items-center justify-content-center gap-[20px]">
-            <h1 class="title-hero">
-                Let's Explore Wonosobo
-            </h1>
-            <p class="subtitle-hero">
-                Pesan dan jelajahi destinasi baru dengan mudah dari mana saja
-            </p>
+        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide-to="0" class="active"
+                    aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide-to="1"
+                    aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide-to="2"
+                    aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="{{ asset('images/wisata/bukitcinta.webp') }}" alt="...">
+                    <div class="carousel-caption">
+                        <h1>Wisata Alam</h1>
+                        <p>Kami menyediakan berbagai destinasi wisata yang menarik</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="{{ asset('images/wisata/kulinerwonogiri.jpeg') }}" alt="...">
+                    <div class="carousel-caption">
+                        <h1>Wisata Kuliner</h1>
+                        <p>Kami menyediakan berbagai destinasi tempat kuliner yang menarik</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="{{ asset('images/events/event1.jpeg') }}" alt="...">
+                    <div class="carousel-caption">
+                        <h1>Event</h1>
+                        <p>Kami memberikan informasi tentang event yang menarik</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
     {{-- End Hero Section --}}
@@ -36,7 +57,7 @@
                 Informasi apa yang anda cari ?
             </h1>
             <div class="d-flex gap-3 mt-3">
-                <a href="{{route('destination.category')}}"
+                <a href="{{ route('destination.category') }}"
                     class="d-flex flex-column gap-1 align-items-center text-decoration-none text-black">
                     <div class="information-image">
                         <img src="{{ asset('images/logos/destinasilogo.png') }}" alt="" />
@@ -167,30 +188,90 @@
                         </a>
                     </div>
                 </div>
-                <div class="container mt-4">
-                    <div class="card p-2 rounded-6" onclick="window.location.href='/event'" style="background-color: #EDEDFF;">
-                        <h3 class="fw-medium mb-0 text-center" style="font-size: 1.25rem; color: black; padding: 1rem">Event</h3>
-                        <div class="d-flex flex-column align-items-center gap-1">
-                            @foreach ($events as $event)
-                                <div class="card mb-3 card-event" onclick="window.location.href='/event/{{ $event->id }}/detail'" style="max-width: 50rem; background-color: #d2d0d07b;">
-                                    <div class="row g-0">
-                                        <div class="col-md-4">
-                                            <img src="{{ asset('storage/' . $event->image) }}" class="img-fluid"
-                                                alt="{{ $event->nama_event }}" style="max-width: 100%; height: auto;">
-                                        </div>
-                                        <div class="col-md-8">
-                                            <div class="card-body">
-                                                <h5 class="card-title" style="font-size: 1rem;">{{ $event->nama_event }}
-                                                </h5>
-                                                <p class="card-text"><small class="text-body-secondary">
-                                                        {{ $event->tanggal_mulai }} -
-                                                        {{ $event->tanggal_selesai }}</small></p>
-                                            </div>
-                                        </div>
+                <div class="d-flex flex-column gap-3 p-2 w-100 w-md-50">
+                    <h1 class="text-center fw-normal rekomendasi-title">Kalender Event</h1>
+                    <div id="carouselCalender" class="carousel slide h-100" data-bs-ride="carousel">
+                        <div class="carousel-inner h-100">
+                            <div class="carousel-item active h-100">
+                                <div class="card h-100 rounded-4">
+                                    <h5 class="card-header text-center bg-primary text-white">January</h5>
+                                    <div class="card-body d-flex flex-column gap-3 px-5">
+                                        <a href=""
+                                            class="d-flex align-items-center text-decoration-none text-black">
+                                            <i
+                                                class="bi bi-calendar d-flex align-items-center justify-content-center me-2 fw-bold fs-2 position-relative">
+                                                <span class="position-absolute fs-6">5</span>
+                                            </i>
+                                            Grebeg Sudiro Tahun
+                                        </a>
                                     </div>
                                 </div>
-                            @endforeach
+                            </div>
+                            <div class="carousel-item  h-100">
+                                <div class="card h-100 rounded-4">
+                                    <h5 class="card-header text-center bg-primary text-white">February</h5>
+                                    <div class="card-body d-flex flex-column gap-3 px-5">
+                                        <a href=""
+                                            class="d-flex align-items-center text-decoration-none text-black">
+                                            <i
+                                                class="bi bi-calendar d-flex align-items-center justify-content-center me-2 fw-bold fs-2 position-relative">
+                                                <span class="position-absolute fs-6">10</span>
+                                            </i>
+                                            Cap Go Meh Singkawang
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="carousel-item  h-100">
+                                <div class="card h-100 rounded-4">
+                                    <h5 class="card-header text-center bg-primary text-white">March</h5>
+                                    <div class="card-body d-flex flex-column gap-3 px-5">
+                                        <a href=""
+                                            class="d-flex align-items-center text-decoration-none text-black">
+                                            <i
+                                                class="bi bi-calendar d-flex align-items-center justify-content-center me-2 fw-bold fs-2 position-relative">
+                                                <span class="position-absolute fs-6">10</span>
+                                            </i>
+                                            Sabang Marine Festival
+                                        </a>
+                                        <a href=""
+                                            class="d-flex align-items-center text-decoration-none text-black">
+                                            <i
+                                                class="bi bi-calendar d-flex align-items-center justify-content-center me-2 fw-bold fs-2 position-relative">
+                                                <span class="position-absolute fs-6">17</span>
+                                            </i>
+                                            Batam WonderFood Festival
+                                        </a>
+                                        <a href=""
+                                            class="d-flex align-items-center text-decoration-none text-black">
+                                            <i
+                                                class="bi bi-calendar d-flex align-items-center justify-content-center me-2 fw-bold fs-2 position-relative">
+                                                <span class="position-absolute fs-6">20</span>
+                                            </i>
+                                            Festival Arakan Sahur
+                                        </a>
+                                        <a href=""
+                                            class="d-flex align-items-center text-decoration-none text-black">
+                                            <i
+                                                class="bi bi-calendar d-flex align-items-center justify-content-center me-2 fw-bold fs-2 position-relative">
+                                                <span class="position-absolute fs-6">30</span>
+                                            </i>
+                                            Pesta Adat Dan Budaya Lom Plai
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselCalender"
+                            data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselCalender"
+                            data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
                     </div>
                 </div>
             </div>
