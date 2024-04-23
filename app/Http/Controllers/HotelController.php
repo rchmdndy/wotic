7b079c6f59e09c1612489c48bb6097b63b16c978
Aji Ananta <ajiananta177013@students.unnes.ac.id>
2024-04-23 19:28:34 +0700

    fix: HotelController

--- app/Http/Controllers/HotelController.php
+++ app/Http/Controllers/HotelController.php
@@ -7,20 +7,8 @@ use Illuminate\Http\Request;
 
 class HotelController extends Controller
 {
-    public function create(){
-        $hotel = new Hotel();
-        $hotel->nama_hotel = "Test Hotel";
-        $hotel->alamat = "Test Alamat";
-        $hotel->image = 'sdlkfjsldkfj';
-        $hotel->kelas = "Bintang 5";
-        $hotel->koordinat = "sekian sekian";
-        $hotel->jarak = "sekian sekian";
-        $hotel->koordinat_y = 'sekian-sekian';
-        $hotel->save();
-        return(view('berhasil'));
-    }
-
-    public function edit(){
-
+    public function index()
+    {
+        return view('hotel.list');
     }
 }
