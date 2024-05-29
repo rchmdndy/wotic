96028e4ff9d0f24dbfe00ad652d0afb8426e0eb2
Davin Fausta <davinfausta@outlook.co.id>
2024-05-29 11:07:37 +0700

    style: list.hotel

--- resources/views/hotel/list.blade.php
+++ resources/views/hotel/list.blade.php
@@ -4,44 +4,47 @@
 @endsection
 @section('custom-content')
 
-<section class="light">
-    <div class="container py-2">
-        <div class="h1 text-center text-dark" id="pageHeaderTitle">Hotel</div>
-        @foreach($hotelList as $hotel)
-                <article class="postcard light blue">
-                    <a class="postcard__img_link" href="#">
-                        <img class="postcard__img" src="{{ $hotel['image'] }}" alt="Image Title" />
-                    </a>
-                    <div class="postcard__text t-dark">
-                        <h1 class="postcard__title blue"><a href="#">{{ $hotel['nama_hotel'] }}</a></h1>
-                        <div class="postcard__subtitle">
-                            <span class="badge rounded-pill text-bg-primary">
-                                <i class="fas fa-calendar-alt mr-2"></i>{{ $hotel['kelas'] }}
-                            </span>
-                        </div>
-                        @if($hotel['is_important'] == 'yes')
-                        <div class="postcard__badge">
-                                <span class="badge rounded-pill text-bg-warning">Rekomendasi</span>
-                        </div>
-                        @endif
-                        <div class="postcard__bar"></div>
-                        <div class="postcard__preview-txt">
-                            {{ $hotel['alamat'] }}
-                        </div>
-                        <div class="my-2">
-                            <a class="btn btn-my-primary w-25"
-                                    href="{{
+    <section class="light">
+        <div class="container py-2">
+            <div class="h1 text-center text-dark" id="pageHeaderTitle">Hotel</div>
+            <div class="row">
+                @foreach($hotelList as $hotel)
+                    <div class="col-md-6 mb-4">
+                        <article class="postcard light blue">
+                            <a class="postcard__img_link" href="#">
+                                <img class="postcard__img" src="{{ $hotel['image'] }}" alt="Image Title" />
+                            </a>
+                            <div class="postcard__text t-dark">
+                                <h1 class="postcard__title blue"><a href="#">{{ $hotel['nama_hotel'] }}</a></h1>
+                                <div class="postcard__subtitle">
+                                <span class="badge rounded-pill text-bg-primary">
+                                    <i class="fas fa-calendar-alt mr-2"></i> {{ $hotel['kelas'] }}
+                                </span>
+                                </div>
+                                @if($hotel['is_important'] == 'yes')
+                                    <div class="postcard__badge">
+                                        <span class="badge rounded-pill text-bg-warning">Rekomendasi</span>
+                                    </div>
+                                @endif
+                                <div class="postcard__bar"></div>
+                                <div class="postcard__preview-txt">
+                                    {{ $hotel['alamat'] }}
+                                </div>
+                                <div class="my-2">
+                                    <a class="btn btn-my-primary w-100"
+                                       href="{{
                                         route('map_redirect',
                                         ['lat' => $hotel['koordinat'],
                                         'lng' => $hotel['koordinat_y']])
-                                    }}", target="_blank">
-                                Lihat Lokasi
-                            </a>
-                        </div>
+                                    }}" target="_blank">
+                                        Lihat Lokasi
+                                    </a>
+                                </div>
+                            </div>
+                        </article>
                     </div>
-                </article>
-        @endforeach
-
-    </div>
-</section>
+                @endforeach
+            </div>
+        </div>
+    </section>
 @endsection
